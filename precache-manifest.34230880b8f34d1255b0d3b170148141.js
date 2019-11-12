458f42e0951c574d57a19da9131699445ba2134b
Shurkodr <sashaglazunov2@gmail.com>
2019-11-12 15:24:04 +0200

    Updates

---
+++ precache-manifest.34230880b8f34d1255b0d3b170148141.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18fba234e01e0da83a18a24273b46a80",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "9a40c56b4b71c25a4d5f",
+    "url": "/react_tabs/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "dad80515a820c51d8df0",
+    "url": "/react_tabs/static/js/2.6ad4a00f.chunk.js"
+  },
+  {
+    "revision": "9a40c56b4b71c25a4d5f",
+    "url": "/react_tabs/static/js/main.2a90da35.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
